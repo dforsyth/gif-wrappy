7549afa167999970c543cf62b1a6ee5cedc57c03
David Forsythe <dforsythe@gmail.com>
2010-10-06 00:35:27 -0700

    add config

---
+++ gwconfig.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+gw_name = 'gif-wrappy'
+gw_version = '0.5a'
+gw_codename = 'R.I.P. Google Wave.'
+
+hello_message = '\n%s\n%s\n%s' % (gw_name, gw_version, gw_codename)
+
+image_url = None
+profile_url = None
+
+blacklist = ['image', 'hosts', 'you', 'dont', 'want', 'to', 'allow']
+
+notag = ['words', 'you', 'think', 'are', 'naughty']
+
+wconfig = { 'admin_pw': 'password',
+            'blacklist': blacklist,
+            'notag': notag,
+            'image_re': r'>>(.*?)\.',
+            'command_re': r'::(.*?)::',
+            'enforce_bans': False }
